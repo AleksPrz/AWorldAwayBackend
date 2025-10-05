23796911d2586e501f9d16bb397327529cf121ca
AleksPrz <alexis4g2@outlook.com>
2025-10-05 03:38:35 -0600

    support cookies

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from routes import route_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 app.register_blueprint(route_bp)
 
 if __name__ == '__main__':
